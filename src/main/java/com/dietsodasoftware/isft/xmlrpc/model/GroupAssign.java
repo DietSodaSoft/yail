600f11020363900f8ad3dff526a73d4d79b393e7
Wendel Schultz <wendel.schultz@infusionsoft.com>
2013-02-25 20:31:20 -0700

    add GroupAssign model

---
+++ src/main/java/com/dietsodasoftware/isft/xmlrpc/model/GroupAssign.java
@@ -0,0 +1,43 @@
+package com.dietsodasoftware.isft.xmlrpc.model;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: wendel.schultz
+ * Date: 2/25/13
+ * Time: 8:29 PM
+ */
+public class GroupAssign extends Model {
+
+    public GroupAssign(Map<String, Object> model) {
+        super(model);
+    }
+
+    @Override
+    public Collection<? extends NamedField> allFields(){
+        return Collections.unmodifiableCollection(Arrays.asList(Field.values()));
+    }
+
+    public enum Field implements NamedField {
+        Id(Integer.class),
+        UserId(Integer.class),
+        GroupId(Integer.class),
+        Admin(Integer.class)
+        ;
+
+        private final Class<?> fieldClass;
+
+        private Field(Class<?> fieldClass) {
+            this.fieldClass = fieldClass;
+        }
+
+        @Override
+        public Class<?> typeClass() {
+            return fieldClass;
+        }
+    }
+}
