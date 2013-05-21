302b376c01d2645c7f05cd54df3b2bd45832392b
Wendel Schultz <wendel.schultz@infusionsoft.com>
2013-05-21 01:54:23 -0700

    model builder for prototype instances

--- src/main/java/com/dietsodasoftware/yail/xmlrpc/model/Model.java
+++ src/main/java/com/dietsodasoftware/yail/xmlrpc/model/Model.java
@@ -3,8 +3,10 @@ package com.dietsodasoftware.yail.xmlrpc.model;
 import com.dietsodasoftware.yail.xmlrpc.client.annotations.TableName;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -62,6 +64,38 @@ public abstract class Model {
             return tableAnnotation.table();
         } else {
             throw new IllegalArgumentException("Must provide the return type Model class annotated by @TableName on class " + modelClass.getName());
+        }
+    }
+
+
+    public static class Builder<M extends Model>{
+        private final Map<String, Object> values = new HashMap<String, Object>();
+        private final Class<M> modelClass;
+        public Builder(Class<M> modelClass){
+            this.modelClass = modelClass;
+        }
+
+        public <T> Builder<M> setFieldValue(NamedField field, T value){
+            values.put(field.name(), value);
+            return this;
+        }
+
+        public <T> Builder<M> setCustomFieldValue(String customFieldName, T value) {
+            values.put("_" + customFieldName, value);
+            return this;
+        }
+
+        public M build(){
+            final Constructor<M> mapConstructor = getModelMapConstructor(modelClass);
+            try {
+                return mapConstructor.newInstance(values);
+            } catch (InstantiationException e) {
+                throw new RuntimeException("Unable to create new instance of " + modelClass, e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException("Unable to create new instance of " + modelClass, e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException("Unable to create new instance of " + modelClass, e);
+            }
         }
     }
 
