91a904241637e132ab0ac48feaa487f5f1569866
Wendel Schultz <wendel.schultz@infusionsoft.com>
2013-05-23 23:12:55 -0700

    batches and batched operations.

---
+++ src/main/java/com/dietsodasoftware/yailext/xmlrpc/client/Batch.java
@@ -0,0 +1,153 @@
+package com.dietsodasoftware.yailext.xmlrpc.client;
+
+import com.dietsodasoftware.yail.xmlrpc.client.YailClient;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ *A batch is a serial list of operations to perform in the order added.
+ *
+ *
+ *
+ * Created with IntelliJ IDEA.
+ * User: wendel.schultz
+ * Date: 5/23/13
+ * Time: 5:26 PM
+ */
+public class Batch {
+
+    public enum Status{
+        NOT_STARTED, RUNNING, FINISHED, ABEND;
+    }
+
+    private final List<BatchedOperation> operations;
+    private final List<BatchedOperation> completedOperations;
+
+    private BatchedOperation preemptiveOperation = null;
+    private Status status = Status.NOT_STARTED;
+    private final AtomicBoolean halt = new AtomicBoolean(false);
+    private final AtomicBoolean isExecuting = new AtomicBoolean(false);
+
+    private Exception haltException = null;
+
+
+    public Batch() {
+        operations = new CopyOnWriteArrayList<BatchedOperation>();
+        completedOperations = new CopyOnWriteArrayList<BatchedOperation>();
+    }
+
+    public Batch addOperationToBatch(BatchedOperation operation){
+        operations.add(operation);
+        return this;
+    }
+
+    public Batch injectImmediateOperationIntoBatch(BatchedOperation operation){
+        if(preemptiveOperation != null){
+            throw new IllegalStateException("Only one operation can preempt");
+        }
+
+        preemptiveOperation = operation;
+        return this;
+    }
+
+    public Batch halt(){
+        halt.set(true);
+        return this;
+    }
+
+    public Batch haltWithException(Exception e){
+        halt();
+        haltException = e;
+
+        return this;
+    }
+
+    public boolean didAbend(){
+        return status == Status.ABEND;
+    }
+
+    public Status getStatus(){
+        return status;
+    }
+
+    public Exception getHaltException(){
+        return haltException;
+    }
+
+    public List<BatchedOperation> getCompletedOperations(){
+        return Collections.unmodifiableList(completedOperations);
+    }
+
+
+    public void execute(YailClient client){
+        if(isExecuting.getAndSet(true)){
+            throw new RuntimeException("Cannot re-execute a batch while it is processing the batch");
+        }
+
+        this.status = Status.RUNNING;
+
+        // re-read the iterator to see if new operations came in during execution
+        while(operations.iterator().hasNext()){
+
+            final Iterator<BatchedOperation> iterator = operations.iterator();
+            operations.clear();
+
+            while(iterator.hasNext()) {
+                if(halt.get()){
+                    this.status = Status.ABEND;
+                    return;
+                }
+
+                final BatchedOperation operation = iterator.next();
+
+                if(preemptiveOperation != null){
+                    final BatchedOperation preemptiveOperation = this.preemptiveOperation;
+                    this.preemptiveOperation = null;
+
+                    performOperation(client, preemptiveOperation);
+                }
+
+                performOperation(client, operation);
+
+            }
+
+        }
+
+        if(preemptiveOperation != null){
+            final BatchedOperation preemptiveOperation = this.preemptiveOperation;
+            this.preemptiveOperation = null;
+
+            performOperation(client, preemptiveOperation);
+        }
+
+        this.status = Status.FINISHED;
+    }
+
+    private void performOperation(YailClient client, BatchedOperation operation){
+        boolean success = true;
+        Object result = null;
+        try {
+            result = client.call(operation.getRpcOperation());
+        } catch (Exception e) {
+            success = false;
+            operation.getCompletion().onError(this, e);
+        } finally {
+            completedOperations.add(operation);
+        }
+
+        if(success){
+            try {
+                operation.getCompletion().onSuccess(this, result);
+            } catch (Exception e){
+                // WTD? operation completed successfully but the callback failed ... should the batch fail?
+                // right now, I say no
+            }
+        }
+
+    }
+
+}
---
+++ src/test/java/com/dietsodasoftware/yailext/xmlrpc/client/BatchTest.java
@@ -0,0 +1,327 @@
+package com.dietsodasoftware.yailext.xmlrpc.client;
+
+import com.dietsodasoftware.yail.xmlrpc.client.YailClient;
+import com.dietsodasoftware.yail.xmlrpc.service.InfusionsoftXmlRpcException;
+import com.dietsodasoftware.yail.xmlrpc.service.InfusionsoftXmlRpcServiceOperation;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: wendel.schultz
+ * Date: 5/23/13
+ * Time: 6:23 PM
+ */
+public class BatchTest {
+
+    @Mock
+    private YailClient client;
+
+    @Mock
+    BatchedOperation.Completion completion1 = mock(BatchedOperation.Completion.class);
+    @Mock
+    InfusionsoftXmlRpcServiceOperation request1 = mock(InfusionsoftXmlRpcServiceOperation.class);
+
+    @Mock
+    BatchedOperation.Completion completion2 = mock(BatchedOperation.Completion.class);
+    @Mock
+    InfusionsoftXmlRpcServiceOperation request2 = mock(InfusionsoftXmlRpcServiceOperation.class);
+
+    @Mock
+    BatchedOperation.Completion completion3 = mock(BatchedOperation.Completion.class);
+    @Mock
+    InfusionsoftXmlRpcServiceOperation request3 = mock(InfusionsoftXmlRpcServiceOperation.class);
+
+    Batch batch;
+
+    @Before
+    public void before() throws InfusionsoftXmlRpcException {
+
+        batch = new Batch();
+
+        MockitoAnnotations.initMocks(this);
+
+        when(client.call(eq(request1))).thenReturn(11);
+        when(client.call(eq(request2))).thenReturn(22);
+        when(client.call(eq(request3))).thenReturn(33);
+
+    }
+
+    @Test
+    public void testBatchWithNoOperationsAddedDuringExecution() throws InfusionsoftXmlRpcException {
+
+        batch.addOperationToBatch(new BatchedOperation(request1, completion1));
+        batch.addOperationToBatch(new BatchedOperation(request2, completion2));
+        batch.addOperationToBatch(new BatchedOperation(request3, completion3));
+
+        // now do work
+        batch.execute(client);
+
+        final InOrder inOrder = inOrder(completion1, completion2, completion3);
+        inOrder.verify(completion1).onSuccess(eq(batch), eq(11));
+        inOrder.verify(completion2).onSuccess(eq(batch), eq(22));
+        inOrder.verify(completion3).onSuccess(eq(batch), eq(33));
+
+        assertFalse("Batch should NOT have abended", batch.didAbend());
+    }
+
+    @Test
+    public void testBatchWithRegularOperationsAddedDuringExecution() throws InfusionsoftXmlRpcException {
+
+        final BatchedOperation.Completion completion4 = new BatchedOperation.Completion(){
+
+            @Override
+            public void onSuccess(Batch batch, Object response) {
+                batch.addOperationToBatch(new BatchedOperation(request3, completion3));
+                assertEquals(44, response);
+            }
+
+            @Override
+            public void onError(Batch batch, Exception e) {
+               throw new IllegalStateException("Should't fail");
+            }
+        };
+        final InfusionsoftXmlRpcServiceOperation request4 = mock(InfusionsoftXmlRpcServiceOperation.class);
+        when(client.call(eq(request4))).thenReturn(44);
+
+        batch.addOperationToBatch(new BatchedOperation(request1, completion1));
+        batch.addOperationToBatch(new BatchedOperation(request4, completion4));
+        batch.addOperationToBatch(new BatchedOperation(request2, completion2));
+
+        batch.execute(client);
+
+        final InOrder inOrder = inOrder(completion1, completion2, completion3);
+        inOrder.verify(completion1).onSuccess(eq(batch), eq(11));
+        inOrder.verify(completion2).onSuccess(eq(batch), eq(22));
+        inOrder.verify(completion3).onSuccess(eq(batch), eq(33));
+
+        assertFalse("Batch should NOT have abended", batch.didAbend());
+    }
+
+    @Test
+    public void testBatchWithPreemptiveOperationsAddedDuringExecution() throws InfusionsoftXmlRpcException {
+
+        final BatchedOperation.Completion completion4 = new BatchedOperation.Completion(){
+
+            @Override
+            public void onSuccess(Batch batch, Object response) {
+                batch.injectImmediateOperationIntoBatch(new BatchedOperation(request3, completion3));
+                assertEquals(44, response);
+            }
+
+            @Override
+            public void onError(Batch batch, Exception e) {
+                throw new IllegalStateException("Should't fail");
+            }
+        };
+        final InfusionsoftXmlRpcServiceOperation request4 = mock(InfusionsoftXmlRpcServiceOperation.class);
+        when(client.call(eq(request4))).thenReturn(44);
+
+        batch.addOperationToBatch(new BatchedOperation(request1, completion1));
+        batch.addOperationToBatch(new BatchedOperation(request4, completion4));
+        batch.addOperationToBatch(new BatchedOperation(request2, completion2));
+
+        batch.execute(client);
+
+        final InOrder inOrder = inOrder(completion1, completion2, completion3);
+        inOrder.verify(completion1).onSuccess(eq(batch), eq(11));
+        inOrder.verify(completion3).onSuccess(eq(batch), eq(33));
+        inOrder.verify(completion2).onSuccess(eq(batch), eq(22));
+
+        assertFalse("Batch should NOT have abended", batch.didAbend());
+    }
+
+    @Test
+    public void testBatchWitPreemptiveOperationsAddedAtEndOfExecution() throws InfusionsoftXmlRpcException {
+
+        final BatchedOperation.Completion completion4 = new BatchedOperation.Completion(){
+
+            @Override
+            public void onSuccess(Batch batch, Object response) {
+                batch.injectImmediateOperationIntoBatch(new BatchedOperation(request3, completion3));
+                assertEquals(44, response);
+            }
+
+            @Override
+            public void onError(Batch batch, Exception e) {
+                throw new IllegalStateException("Should't fail");
+            }
+        };
+        final InfusionsoftXmlRpcServiceOperation request4 = mock(InfusionsoftXmlRpcServiceOperation.class);
+        when(client.call(eq(request4))).thenReturn(44);
+
+        batch.addOperationToBatch(new BatchedOperation(request1, completion1));
+        batch.addOperationToBatch(new BatchedOperation(request2, completion2));
+        batch.addOperationToBatch(new BatchedOperation(request4, completion4));
+
+        batch.execute(client);
+
+        final InOrder inOrder = inOrder(completion1, completion2, completion3);
+        inOrder.verify(completion1).onSuccess(eq(batch), eq(11));
+        inOrder.verify(completion2).onSuccess(eq(batch), eq(22));
+        inOrder.verify(completion3).onSuccess(eq(batch), eq(33));
+
+        assertFalse("Batch should NOT have abended", batch.didAbend());
+    }
+
+    @Test
+    public void testBatchWithRegularOperationsHaltedPrematurelyDuringExecution() throws InfusionsoftXmlRpcException {
+
+        final BatchedOperation.Completion completion4 = new BatchedOperation.Completion(){
+
+            @Override
+            public void onSuccess(Batch batch, Object response) {
+                batch.halt();
+                assertEquals(44, response);
+            }
+
+            @Override
+            public void onError(Batch batch, Exception e) {
+                throw new IllegalStateException("Should't fail");
+            }
+        };
+        final InfusionsoftXmlRpcServiceOperation request4 = mock(InfusionsoftXmlRpcServiceOperation.class);
+        when(client.call(eq(request4))).thenReturn(44);
+
+        final BatchedOperation first = new BatchedOperation(request1, completion1);
+        final BatchedOperation second = new BatchedOperation(request2, completion2);
+        final BatchedOperation third = new BatchedOperation(request3, completion3);
+        final BatchedOperation halt = new BatchedOperation(request4, completion4);
+        batch.addOperationToBatch(first);
+        batch.addOperationToBatch(second);
+        batch.addOperationToBatch(halt);
+        batch.addOperationToBatch(third);
+
+        batch.execute(client);
+
+        final InOrder inOrder = inOrder(completion1, completion2);
+        inOrder.verify(completion1).onSuccess(eq(batch), eq(11));
+        inOrder.verify(completion2).onSuccess(eq(batch), eq(22));
+        verifyZeroInteractions(completion3);
+
+                assertTrue(batch.getCompletedOperations().contains(first));
+        assertTrue(batch.getCompletedOperations().contains(second));
+        assertTrue(batch.getCompletedOperations().contains(halt));
+        assertFalse(batch.getCompletedOperations().contains(third));
+        assertEquals(3, batch.getCompletedOperations().size());
+        assertTrue("Batch should have abended", batch.didAbend());
+    }
+
+    @Test
+    public void testBatchWithPreemptiveOperationsHaltedPrematurelyDuringExecution() throws InfusionsoftXmlRpcException {
+
+        final BatchedOperation first = new BatchedOperation(request1, completion1);
+        final BatchedOperation second = new BatchedOperation(request2, completion2);
+        final BatchedOperation third = new BatchedOperation(request3, completion3);
+
+        final InfusionsoftXmlRpcServiceOperation request4 = mock(InfusionsoftXmlRpcServiceOperation.class);
+        final BatchedOperation.Completion completion4 = new BatchedOperation.Completion(){
+
+            @Override
+            public void onSuccess(Batch batch, Object response) {
+                batch.injectImmediateOperationIntoBatch(third);
+                batch.halt();
+                assertEquals(44, response);
+            }
+
+            @Override
+            public void onError(Batch batch, Exception e) {
+                throw new IllegalStateException("Should't fail");
+            }
+        };
+        when(client.call(eq(request4))).thenReturn(44);
+
+        final BatchedOperation halt = new BatchedOperation(request4, completion4);
+
+        batch.addOperationToBatch(first);
+        batch.addOperationToBatch(halt);
+        batch.addOperationToBatch(second);
+
+        batch.execute(client);
+
+        final InOrder inOrder = inOrder(completion1);
+        inOrder.verify(completion1).onSuccess(eq(batch), eq(11));
+        verifyZeroInteractions(completion2);
+        verifyZeroInteractions(completion3);
+
+
+        assertTrue(batch.getCompletedOperations().contains(first));
+        assertFalse(batch.getCompletedOperations().contains(second));
+        assertTrue(batch.getCompletedOperations().contains(halt));
+        assertFalse(batch.getCompletedOperations().contains(third));
+        assertEquals(2, batch.getCompletedOperations().size());
+
+        assertTrue("Batch should have abended", batch.didAbend());
+    }
+
+    @Test
+    public void testBatchWithRegularOperationsAddedDuringExecutionHaltedAfterLastOperationCompleted() throws InfusionsoftXmlRpcException {
+
+        final BatchedOperation second = new BatchedOperation(request2, completion2);
+        final BatchedOperation third = new BatchedOperation(request3, completion3);
+
+        final BatchedOperation.Completion completion1 = spy(new BatchedOperation.Completion() {
+            @Override
+            public void onSuccess(Batch batch, Object response) {
+                batch.injectImmediateOperationIntoBatch(second);
+            }
+
+            @Override
+            public void onError(Batch batch, Exception e) {
+                throw new IllegalArgumentException("Shouldn't error out");
+            }
+        });
+
+        final InfusionsoftXmlRpcServiceOperation request4 = mock(InfusionsoftXmlRpcServiceOperation.class);
+        final BatchedOperation.Completion completion4 = new BatchedOperation.Completion(){
+
+            @Override
+            public void onSuccess(Batch batch, Object response) {
+                batch.halt();
+                assertEquals(44, response);
+            }
+
+            @Override
+            public void onError(Batch batch, Exception e) {
+                throw new IllegalStateException("Should't fail");
+            }
+        };
+        when(client.call(eq(request4))).thenReturn(44);
+
+        final BatchedOperation first = new BatchedOperation(request1, completion1);
+        final BatchedOperation halt = new BatchedOperation(request4, completion4);
+
+        batch.addOperationToBatch(first);
+        batch.addOperationToBatch(third);
+        batch.addOperationToBatch(halt);
+
+        batch.execute(client);
+
+        final InOrder inOrder = inOrder(completion1, completion2, completion3);
+        inOrder.verify(completion1).onSuccess(eq(batch), eq(11));
+        inOrder.verify(completion2).onSuccess(eq(batch), eq(22));
+        inOrder.verify(completion3).onSuccess(eq(batch), eq(33));
+
+        assertTrue(batch.getCompletedOperations().contains(first));
+        assertTrue(batch.getCompletedOperations().contains(second));
+        assertTrue(batch.getCompletedOperations().contains(third));
+        assertTrue(batch.getCompletedOperations().contains(halt));
+        assertEquals(4, batch.getCompletedOperations().size());
+
+        assertFalse("Batch should NOT have abended", batch.didAbend());
+    }
+
+}
